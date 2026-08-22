063a7a0b19eddfd787ce55bc355e998a1287200e
newstep209-alt <newstep209@gmail.com>
2026-08-22 17:15:02 +0000

    Simplify main.py by removing bgcolor and updating text
    Removed bgcolor setting and adjusted text addition.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 import flet as ft
 def main(page: ft.Page):
     page.title = "TEST VIDE"
-    page.bgcolor = "white"
-    page.add(ft.Text("CA MARCHE ENFIN", size=40, color="green"))
-
+    page.add(ft.Text("CA MARCHE ENFIN"))
 ft.app(target=main)
